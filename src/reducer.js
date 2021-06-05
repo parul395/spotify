e6b1287fd83c74937f6ed345978afca9e6ccc665
parul395 <parul.11804638@gmail.com>
2021-06-05 12:30:20 +0530

    initial commit

---
+++ src/reducer.js
@@ -0,0 +1,49 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  spotify: null,
+  discover_weekly: null,
+  top_artists: null,
+  playing: false,
+  item: null,
+  token: null,
+};
+
+const reducer = (state, action) => {
+  console.log("action:::", action);
+
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+
+    case "SET_ITEM":
+      return {
+        ...state,
+        item: action.item,
+      };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    case "SET_DISCOVER_WEEKLY":
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
